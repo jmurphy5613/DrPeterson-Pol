c4615f869fd00279053b300ce090887805a4c6e9
jmurphy5613 <jmurphy5613@gmail.com>
2022-06-12 23:24:04 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="root">
+      <h4>AP Calculus</h4>
+      <h1 className="title">Dr.Peterson's personal class poll service</h1>
+      <h3>This is a web application created by John Murphy, and serves as my final project</h3>
+    </div>
+  );
+}
+
+export default App;
